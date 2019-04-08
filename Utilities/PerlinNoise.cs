00d29f381a30ef48e205c4d504d0043f3f74f9a2
neo <anik_patel@live.com>
2019-04-08 10:45:34 -0400

    Fixed Perlin noise generation

--- Utilities/PerlinNoise.cs
+++ Utilities/PerlinNoise.cs
@@ -2,146 +2,125 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace Utilities {
-    public class PerlinNoise {
-        public static int repeat = -1;
+    public class PerlinNoise { //Based on Herman Tulleken's implementation from Dev.Mag
+                               //http://devmag.org.za/2009/04/25/perlin-noise/
 
-        public PerlinNoise() {
-            repeat++;
+        static Random random = new Random();
+
+        public static float[][] GenerateWhiteNoise(int width, int height) {
+            float[][] noise = GetEmptyArray<float>(width, height);
+
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    noise[i][j] = (float)random.NextDouble() % 1;
+                }
+            }
+
+            return noise;
         }
 
-        public double OctavePerlin(double x, double y, double z, int octaves, double persistence) {
-    double total = 0;
-    double frequency = 1;
-    double amplitude = 1;
-    double maxValue = 0;  // Used for normalizing result to 0.0 - 1.0
-    for(int i=0;i<octaves;i++) {
-        total += perlin(x * frequency, y * frequency, z * frequency) * amplitude;
+        public static float Interpolate(float x0, float x1, float alpha) {
+            return x0 * (1 - alpha) + alpha * x1;
+        }
+
         
-        maxValue += amplitude;
-        
-        amplitude *= persistence;
-        frequency *= 2;
-    }
-    
-    return total/maxValue;
-}
 
-        private static readonly int[] permutation = { 151,160,137,91,90,15,                 // Hash lookup table as defined by Ken Perlin.  This is a randomly
-            131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,    // arranged array of all numbers from 0-255 inclusive.
-            190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-            88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-            77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-            102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-            135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-            5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-            223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-            129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-            251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-            49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-            138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
-         };
+        public static T[][] GetEmptyArray<T>(int width, int height) {
+            T[][] image = new T[width][];
 
-        private static readonly int[] p;
-
-        static PerlinNoise() {
-            p = new int[512];
-            for (int i = 0; i < 512; i++) {
-                p[i] = permutation[i % 256];
+            for (int i = 0; i < width; i++) {
+                image[i] = new T[height];
             }
 
+            return image;
         }
 
-        public static double fade(double t) {
-                                                                // Fade function as defined by Ken Perlin.  This eases coordinate values
-                                                                // so that they will ease towards integral values.  This ends up smoothing
-                                                                // the final output.
-            return t * t * t * (t * (t * 6 - 15) + 10);         // 6t^5 - 15t^4 + 10t^3
-        }
+        public static float[][] GenerateSmoothNoise(float[][] baseNoise, int octave) {
+            int width = baseNoise.Length;
+            int height = baseNoise[0].Length;
 
-        public static double grad(int hash, double x, double y, double z) {
-            switch (hash & 0xF) {
-                case 0x0: return  x + y;
-                case 0x1: return -x + y;
-                case 0x2: return  x - y;
-                case 0x3: return -x - y;
-                case 0x4: return  x + z;
-                case 0x5: return -x + z;
-                case 0x6: return  x - z;
-                case 0x7: return -x - z;
-                case 0x8: return  y + z;
-                case 0x9: return -y + z;
-                case 0xA: return  y - z;
-                case 0xB: return -y - z;
-                case 0xC: return  y + x;
-                case 0xD: return -y + z;
-                case 0xE: return  y - x;
-                case 0xF: return -y - z;
-                default: return 0; // never happens
-            }
-        }
+            float[][] smoothNoise = GetEmptyArray<float>(width, height);
 
-        public static double lerp(double a, double b, double x) {
-            return a + x * (b - a);
-        }
+            int samplePeriod = 1 << octave; // calculates 2 ^ k
+            float sampleFrequency = 1.0f / samplePeriod;
 
-        public static double perlin(double x, double y, double z) {
-            if (repeat > 0) {
-                x %= repeat;
-                y %= repeat;
-                z %= repeat;
+            for (int i = 0; i < width; i++) {
+                //calculate the horizontal sampling indices
+                int sample_i0 = (i / samplePeriod) * samplePeriod;
+                int sample_i1 = (sample_i0 + samplePeriod) % width; //wrap around
+                float horizontal_blend = (i - sample_i0) * sampleFrequency;
+
+                for (int j = 0; j < height; j++) {
+                    //calculate the vertical sampling indices
+                    int sample_j0 = (j / samplePeriod) * samplePeriod;
+                    int sample_j1 = (sample_j0 + samplePeriod) % height; //wrap around
+                    float vertical_blend = (j - sample_j0) * sampleFrequency;
+
+                    //blend the top two corners
+                    float top = Interpolate(baseNoise[sample_i0][sample_j0],
+                        baseNoise[sample_i1][sample_j0], horizontal_blend);
+
+                    //blend the bottom two corners
+                    float bottom = Interpolate(baseNoise[sample_i0][sample_j1],
+                        baseNoise[sample_i1][sample_j1], horizontal_blend);
+
+                    //final blend
+                    smoothNoise[i][j] = Interpolate(top, bottom, vertical_blend);
+                }
             }
 
-            int xi = (int)x & 255;
-            int yi = (int)y & 255;
-            int zi = (int)z & 255;
-
-            double xf = x - (int)x;
-            double yf = y - (int)y;
-            double zf = z - (int)z;
-
-            double u = fade(xf);
-            double v = fade(yf);
-            double w = fade(zf);
-
-            int aaa, aba, aab, abb, baa, bba, bab, bbb;
-            aaa = p[p[p[    xi ] +     yi ] +     zi ];
-            aba = p[p[p[    xi ] + inc(yi)] +     zi ];
-            aab = p[p[p[    xi ] +     yi ] + inc(zi)];
-            abb = p[p[p[    xi ] + inc(yi)] + inc(zi)];
-            baa = p[p[p[inc(xi)] +     yi ] +     zi ];
-            bba = p[p[p[inc(xi)] + inc(yi)] +     zi ];
-            bab = p[p[p[inc(xi)] +     yi ] + inc(zi)];
-            bbb = p[p[p[inc(xi)] + inc(yi)] + inc(zi)];
-
-            double x1, x2, y1, y2;
-            x1 = lerp(grad(aaa, xf, yf, zf),                                        // The gradient function calculates the dot product between a pseudorandom
-                        grad(baa, xf - 1, yf, zf),                                  // gradient vector and the vector from the input coordinate to the 8
-                        u);                                                         // surrounding points in its unit cube.
-            x2 = lerp(grad(aba, xf, yf - 1, zf),                                    // This is all then lerped together as a sort of weighted average based on the faded (u,v,w)
-                        grad(bba, xf - 1, yf - 1, zf),                              // values we made earlier.
-                          u);
-            y1 = lerp(x1, x2, v);
-
-            x1 = lerp(grad(aab, xf, yf, zf - 1),
-                        grad(bab, xf - 1, yf, zf - 1),
-                        u);
-            x2 = lerp(grad(abb, xf, yf - 1, zf - 1),
-                          grad(bbb, xf - 1, yf - 1, zf - 1),
-                          u);
-            y2 = lerp(x1, x2, v);
-
-            return (lerp(y1, y2, w) + 1) / 2;                                      // For convenience we bind the result to 0 - 1 (theoretical min/max before is [-1, 1])
+            return smoothNoise;
         }
 
-    
+        public static float[][] GeneratePerlinNoise(float[][] baseNoise, int octaveCount) {
+            int width = baseNoise.Length;
+            int height = baseNoise[0].Length;
 
-        public static int inc(int num) {
-            num++;
-            if (repeat > 0) num %= repeat;
+            float[][][] smoothNoise = new float[octaveCount][][]; //an array of 2D arrays containing
 
-            return num;
+            float persistance = 0.7f;
+
+            //generate smooth noise
+            for (int i = 0; i < octaveCount; i++) {
+                smoothNoise[i] = GenerateSmoothNoise(baseNoise, i);
+            }
+
+            float[][] perlinNoise = GetEmptyArray<float>(width, height); //an array of floats initialised to 0
+
+            float amplitude = 1.0f;
+            float totalAmplitude = 0.0f;
+
+            //blend noise together
+            for (int octave = octaveCount - 1; octave >= 0; octave--) {
+                amplitude *= persistance;
+                totalAmplitude += amplitude;
+
+                for (int i = 0; i < width; i++) {
+                    for (int j = 0; j < height; j++) {
+                        perlinNoise[i][j] += smoothNoise[octave][i][j] * amplitude;
+                    }
+                }
+            }
+
+            //normalisation
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    perlinNoise[i][j] /= totalAmplitude;
+                }
+            }
+
+            return perlinNoise;
         }
+
+        public static float[][] GeneratePerlinNoise(int width, int height, int octaveCount) {
+            float[][] baseNoise = GenerateWhiteNoise(width, height);
+
+            return GeneratePerlinNoise(baseNoise, octaveCount);
+        }
+
+              
 
 
     }
